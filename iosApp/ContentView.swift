786384bf661266d268db440dc40345717492b878
janpauldahlke <janpauldahlke@gmail.com>
2024-07-29 14:29:48 +0200

    better order in IOS imports

---
+++ iosApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by Dahlke, Jan on 29.07.24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import shared
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        AboutScreen()
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+}
+
+
--- iosApp/iosApp/AboutListView.swift
+++ iosApp/iosApp/AboutListView.swift
@@ -55,3 +55,8 @@ struct AboutListView: View {
     }
  
 }
+
+
+#Preview {
+    AboutListView()
+}
